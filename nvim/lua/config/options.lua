0092399962ee86ecfe9f8f7ef6ad0cfc81439f3c
heisenberg <nathanneil66@gmail.com>
2024-12-18 03:08:24 +0800

    feat: 添加python环境, 修改snippets

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -32,6 +32,9 @@ vim.opt.mouse = ''
 vim.opt.spelllang = { 'en', 'cjk' }
 vim.opt.spelloptions:append('camel')
 
+vim.g.lazyvim_python_lsp = 'pyright'
+vim.g.lazyvim_python_ruff = 'ruff'
+
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
