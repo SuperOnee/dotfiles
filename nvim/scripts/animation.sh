3efd787e608e76faa93fbf360a1b3346c646124f
heisenberg <nathanneil66@gmail.com>
2025-01-16 12:48:58 +0800

    feat: fix animation.sh file not found

--- nvim/scripts/animation.sh
+++ nvim/scripts/animation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 frames=(
-  "../static/header1.cat"
-  "../static/header2.cat"
-  "../static/header3.cat"
-  "../static/header4.cat")
+  "/Users/heisenberg/.config/nvim/static/header1.cat"
+  "/Users/heisenberg/.config/nvim/static/header2.cat"
+  "/Users/heisenberg/.config/nvim/static/header3.cat"
+  "/Users/heisenberg/.config/nvim/static/header4.cat"
+)
 
 while true; do
   for frame in "${frames[@]}"; do
