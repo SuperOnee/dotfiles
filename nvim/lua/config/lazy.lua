2cdd57128df865f8fee54d02529df4b4bc7832ff
heisenberg <nathanneil66@gmail.com>
2025-01-16 13:01:40 +0800

    feat: Add toml support to neovim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.lang.json' },
     { import = 'lazyvim.plugins.extras.lang.markdown' },
     { import = 'lazyvim.plugins.extras.lang.yaml' },
+    { import = 'lazyvim.plugins.extras.lang.toml' },
     { import = 'lazyvim.plugins.extras.lang.go' },
     { import = 'lazyvim.plugins.extras.lang.python' },
     { import = 'lazyvim.plugins.extras.lang.tailwind' },
