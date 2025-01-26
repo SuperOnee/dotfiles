d98ec7334686750f109d8a75c5662984b40c89ba
heisenberg <nathanneil66@gmail.com>
2025-01-26 11:19:06 +0800

    fix: Change sketchybar ram widget command to vm_stat

--- sketchybar/items/widgets/ram.lua
+++ sketchybar/items/widgets/ram.lua
@@ -39,23 +39,51 @@ sbar.add("item", "widgets.ram.padding", {
 })
 
 ram:subscribe({ "routine", "forced", "system_woke" }, function(env)
-	sbar.exec("memory_pressure", function(output)
-		local percentage = output:match("System%-wide memory free percentage: (%d+)")
-		local load = 100 - tonumber(percentage)
-		ram:push({ load / 100. })
-		local color = colors.blue
-		if load > 30 then
-			if load < 60 then
-				color = colors.yellow
-			elseif load < 80 then
-				color = colors.orange
-			else
-				color = colors.red
-			end
+	sbar.exec("sysctl -n hw.pagesize", function(page_size_output)
+		local page_size = tonumber(page_size_output)
+		if not page_size_output or page_size == 0 then
+			return
 		end
-		ram:set({
-			graph = { color = color },
-			label = { string = "ram " .. load .. "%" },
-		})
+
+		sbar.exec("vm_stat", function(vm_stat_output)
+			if not vm_stat_output then
+				return
+			end
+
+			local pages_free = tonumber(vm_stat_output:match("Pages free:%s+(%d+)")) or 0
+			local pages_active = tonumber(vm_stat_output:match("Pages active:%s+(%d+)")) or 0
+			local pages_inactive = tonumber(vm_stat_output:match("Pages inactive:%s+(%d+)")) or 0
+			local pages_speculative = tonumber(vm_stat_output:match("Pages speculative:%s+(%d+)")) or 0
+			local pages_wired = tonumber(vm_stat_output:match("Pages wired down:%s+(%d+)")) or 0
+			local pages_compressed = tonumber(vm_stat_output:match("Pages occupied by compressor:%s+(%d+)")) or 0
+
+			local total_pages = pages_free
+				+ pages_active
+				+ pages_inactive
+				+ pages_speculative
+				+ pages_wired
+				+ pages_compressed
+			local used_pages = pages_active + pages_wired + pages_compressed + pages_speculative
+
+			local total_memory = total_pages * page_size
+			local used_memory = used_pages * page_size
+			local memory_usage = (used_memory / total_memory) * 100
+
+			local color = colors.blue
+			if memory_usage > 30 then
+				if memory_usage < 60 then
+					color = colors.yellow
+				elseif memory_usage < 80 then
+					color = colors.orange
+				else
+					color = colors.red
+				end
+			end
+
+			ram:set({
+				graph = { color = color },
+				label = { string = "ram " .. string.format("%.0f", memory_usage) .. "%" },
+			})
+		end)
 	end)
 end)
