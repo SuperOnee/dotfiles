64acd211df1688bd53c1375dd46e823d7fa8fce4
heisenberg <nathanneil66@gmail.com>
2025-04-16 20:52:25 +0800

    feat: Add file size display to yazi file manager

--- yazi/init.lua
+++ yazi/init.lua
@@ -3,3 +3,9 @@ require("git"):setup()
 THEME.git = THEME.git or {}
 THEME.git.modified = ui.Style():fg("blue")
 THEME.git.deleted = ui.Style():fg("red"):bold()
+
+-- ~/.config/yazi/init.lua
+function Linemode:size()
+	local size = self._file:size()
+	return size and ya.readable_size(size) or ""
+end
